8483159ac519fefe859d238198414fbf00f0a72b
HelloRocket <info@hellorocket.org>
2024-05-18 17:11:06 +0900

    Adjusted volume controls

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    Volume += -32
+    music.setVolume(Volume)
+})
+input.onButtonPressed(Button.B, function () {
+    Volume += 32
+    music.setVolume(Volume)
+})
+let Volume = 0
+basic.showIcon(IconNames.Rabbit)
+Volume = 127
+music.setVolume(Volume)
 basic.forever(function () {
-	
+    music.setTempo(Math.map(Math.abs(input.acceleration(Dimension.Y)), 0, 1023, 60, 320))
+    music.play(music.tonePlayable(input.lightLevel() * 25, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
 })
